3face66850fdf1871c7a8298e7c389325381da90
sheafk <sheafureyking@gmail.com>
2016-07-05 15:59:38 -0400

    Done.

--- Types.playground/Contents.swift
+++ Types.playground/Contents.swift
@@ -4,13 +4,13 @@ var ninthPlanet: String
 ninthPlanet = "Pluto"
 
 let fifthPlanet: String = "Jupiter"
-// fifthPlanet is a constant of type ______
+// fifthPlanet is a constant of type String
 
 let sixthPlanet: String = "Saturn"
-// sixthPlanet is a constant of type ______
+// sixthPlanet is a constant of type String
 
 let seventhPlanet = "Uranus"
-// seventhPlanet is a constant of type ______
+// seventhPlanet is a constant of type String
 
 let numberOfCountriesInAfrica = 54
 
